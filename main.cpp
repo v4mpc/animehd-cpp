eb88c3ec746bf9e1dec2a3be0a94f743d40b7229
Yona Mahundi <yona_mahundi@tz.jsi.com>
2021-06-14 20:13:23 +0300

    loading glade file successcul

--- main.cpp
+++ main.cpp
@@ -1,12 +1,25 @@
 #include <gtkmm.h>
+#include <iostream>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     Gtk::Main kit(argc, argv);
+    Glib::RefPtr<Gtk::Builder> refBuilder = Gtk::Builder::create();
+    try {
+        refBuilder->add_from_file("../main.glade");
+    }
+    catch (const Glib::FileError &ex) {
+        std::cerr << "FileError: " << ex.what() << std::endl;
+        return 1;
+    }
+    catch (const Gtk::BuilderError &ex) {
+        std::cerr << "BuilderError: " << ex.what() << std::endl;
+        return 1;
+    }
 
-    Gtk::Window window;
+    Gtk::ApplicationWindow *pWindow = nullptr;
+    refBuilder->get_widget("main_window", pWindow);
+    Gtk::Main::run(*pWindow);
 
-    Gtk::Main::run(window);
 
     return EXIT_SUCCESS;
 }
